14d919c5391fb7e076dadf3335ee37c7efa17151
ChenBingWei1201 <rain4030633@gmail.com>
2024-02-01 11:34:41 +0800

    Basic & Data Structure

---
+++ leetcode/Easy/21-MergeTwoSortedLists/mergeTwoSortedLists.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+// Definition for singly-linked list.
+struct ListNode {
+  int val;
+  struct ListNode *next;
+};
+
+struct ListNode *mergeTwoLists(struct ListNode *list1, struct ListNode *list2) {
+
+}
+
+int main() {
+  struct ListNode *list1;
+  struct ListNode *list2;
+  struct ListNode *mergedList = mergeTwoLists(list1, list2);
+  return 0;
+}
