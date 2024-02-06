466346fba7f18ae48cb7cbb49985dfb8ccea4e16
ChenBingWei1201 <rain4030633@gmail.com>
2024-02-06 21:20:51 +0800

    call by address and typedef

--- Basic/lesson-09-function/function.c
+++ Basic/lesson-09-function/function.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 
 int sum(int k);
+int sum_pointer(int *k);
 
 int main() {
-  int result = sum(10);
-  printf("%d", result);
+  int k = 10;
+  int result_pointer = sum_pointer(&k);
+  printf("%d\n", result_pointer);
+  int result = sum(k);
+  printf("%d\n", result);
   return 0;
 }
 
@@ -14,4 +18,12 @@ int sum(int k) {
   } else {
     return 0;
   }
-} 
+}
+
+int sum_pointer(int *k) {
+  if (*k > 0) {
+    return ++(*k); // will change the value of k
+  } else {
+    return 0;
+  }
+}
--- Basic/lesson-11-struct/struct.c
+++ Basic/lesson-11-struct/struct.c
@@ -2,10 +2,16 @@
 #include <string.h>
 
 struct student {
-  char name[20];
+  char *name;
   int age;
   int score;
 };
+
+// use typedef to define a new type
+typedef struct {
+  char *name;
+  int age;
+} person;
 
 int main() {
   struct student st = {"zhangsan", 20, 90};
@@ -14,6 +20,11 @@ int main() {
   st.age = 30;
   st.score = 100;
   printf("name = %s, age = %d, score = %d\n", st.name, st.age, st.score);
+
+  // don't need to use struct keyword
+  person john;
+  john.name = "John";
+  john.age = 25;
+  printf("name = %s, age = %d\n", john.name, john.age);
   return 0;
 }
-
